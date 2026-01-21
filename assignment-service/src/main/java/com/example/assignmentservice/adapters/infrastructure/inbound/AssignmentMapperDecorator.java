cb4be3fdf1c6aaf677dc616a72a495904d440e82
DanielBabenko <danielbabenko606@yandex.ru>
2026-01-21 12:48:41 +0300

    fix: using port for layer communication

--- assignment-service/src/main/java/com/example/assignmentservice/adapters/infrastructure/inbound/AssignmentMapperDecorator.java
+++ assignment-service/src/main/java/com/example/assignmentservice/adapters/infrastructure/inbound/AssignmentMapperDecorator.java
@@ -4,7 +4,9 @@ import com.example.assignmentservice.application.dto.UserProductAssignmentDto;
 import com.example.assignmentservice.application.mapper.AssignmentMapper;
 import com.example.assignmentservice.domain.model.entity.UserProductAssignment;
 import com.example.assignmentservice.domain.ports.AssignmentMapperPort;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AssignmentMapperDecorator implements AssignmentMapperPort {
     private final AssignmentMapper delegate = new AssignmentMapper();
 
